67cee2640ed7b7d4ea6a282685b764d809325607
Andrew Osmond <aosmond3@gatech.edu>
2023-10-21 14:26:09 -0400

    Initial post attempt request

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import data_accessor as da
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -17,7 +17,15 @@ def hello(): # TODO: delete
 def api_get_users():
     return jsonify(da.get_users())
 
-@app.route("/api/users/join/group", methods=["POST"]) # TODO: probably need to modify how I am receiving inputs
-def api_user_join_group(user_uuid: int, group_id: int, group_password: str):
-    # validate password
-    return jsonify(da.get_group_password())
+# @app.route("/api/users/join/group", methods=["POST"]) # TODO: probably need to modify how I am receiving inputs
+# def api_user_join_group(user_uuid: int, group_id: int, group_password: str):
+#     # validate password
+#     return jsonify(da.get_group_password())
+
+@app.route("/api/login", methods=["POST"])
+def api_login():
+    username = request.form["username"]
+    password = request.form["password"]
+    print(username, password)
+
+
